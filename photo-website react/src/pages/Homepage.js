4f5385f2a87039e46d4af04df2d0ee87c9dc77aa
Linda <chailn8808@gmail.com>
2022-04-07 20:50:20 +0800

    portifolio

---
+++ photo-website react/src/pages/Homepage.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import Search from "../components/Search";
+import Picture from "../components/Picture";
+
+const Homepage = () => {
+  const [input, setInput] = useState("");
+  let [data, setDate] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
+  const auth = "563492ad6f91700001000001fa3fedc333574d738d384ddf321ab736";
+  const intialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  const searchURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=1`;
+
+  // fetch data from pexels api
+  const search = async (url) => {
+    setPage(2);
+    const dataFetch = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parsedData = await dataFetch.json();
+    setDate(parsedData.photos);
+  };
+
+  //load more picture
+  const morepicture = async () => {
+    let newURL;
+    if (input === "") {
+      newURL = `https://api.pexels.com/v1/curated?${page}&per_page=15;`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=${page}`;
+    }
+    setPage(page + 1);
+    const dataFetch = await fetch(newURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parsedData = await dataFetch.json();
+    setDate(data.concat(parsedData.photos));
+  };
+
+  // fetch data when the page Loads up
+  useEffect(() => {
+    search(intialURL);
+  }, []);
+
+  useEffect(() => {
+    if (currentSearch === "") {
+      search(intialURL);
+    } else {
+      search(intialURL);
+    }
+    search(searchURL);
+  }, [currentSearch]);
+
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <Search
+        search={() => {
+          // JS Closure
+          setCurrentSearch(input);
+        }}
+        setInput={setInput}
+      />
+      <div className="pictures">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
+
+      <div className="morePicture">
+        <button onClick={morepicture}>Load More</button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
